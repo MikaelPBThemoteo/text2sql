a8d4e60ec419cd02711eddcf5a8a52f670f82eae
hagijakobson <hagijakobson@gmail.com>
2023-05-20 10:55:27 -0300

    Adicionado a seção Sobre

--- app.py
+++ app.py
@@ -2,34 +2,57 @@ from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
 import streamlit as st
 import pandas as pd
 import os   
+from langchain.agents import create_csv_agent
 
-st.title('Ask to Your Database')
+def sqlite():
+    pass
 
-#uploaded_file
-uploaded_file = st.file_uploader("Carregue o seu banco de dados:", type='sqlite')
+def csv():
+    pass
 
-api_key = st.text_input('Digite sua chave da OpenAI API:', '', type='password')
-os.environ["OPENAI_API_KEY"] = api_key
 
-#"List the total sales per country. Which country's customers spent the most?"
-query = st.text_input('Digite sua pergunta:', '')
+with st.sidebar:
+    st.header('Sobre')
+    st.markdown('Obrigado pelo seu interesse na nossa aplicação. Por favor, \
+                esteja ciente de que este é apenas um sistema de Prova de \
+                Conceito e pode conter bugs ou recursos inacabados.')
+    st.markdown('Feito por [Hagi Jakobson]() e [Mikael Timoteo]().')
+    st.markdown('O código fonte pode ser encontrado [aqui]().')
+    
+st.title('Pergunte A Sua Base de Dados')
 
-if st.button('Submeter'):
-    run = (uploaded_file is not None) and (api_key is not None) and (query is not None)
-    if run:
-        # Lê o conteúdo do arquivo carregado em memória
-        content = uploaded_file.read()
+option = st.selectbox('Qual o tipo da sua base de dados?',
+                      ('SQLITE', 'CSV'))
 
-        # Grava o arquivo carregado no disco em um local temporário
-        with open('temp.sqlite', 'wb') as f:
-            f.write(content)
+if option == 'SQLITE':
+    #uploaded_file
+    uploaded_file = st.file_uploader("Carregue a sua base de dados:", 
+                                    type='sqlite')
 
-        db = SQLDatabase.from_uri("sqlite:///temp.sqlite")    
-        llm = OpenAI(temperature=0)
-        db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True, return_intermediate_steps=True)
-        result = db_chain(query)
+    api_key = st.text_input('Digite sua chave da OpenAI API:', '', type='password')
+    os.environ["OPENAI_API_KEY"] = api_key
 
-        st.write("Resposta:")
-        st.write(result['result'])
-        st.write("SQLQuery:")
-        st.write(result['intermediate_steps'][0])
+    #"List the total sales per country. Which country's customers spent the most?"
+    query = st.text_input('Digite sua pergunta:', '')
+
+    if st.button('Submeter'):
+        run = (uploaded_file is not None) and (api_key is not None) and (query is not None)
+        if run:
+            # Lê o conteúdo do arquivo carregado em memória
+            content = uploaded_file.read()
+
+            # Grava o arquivo carregado no disco em um local temporário
+            with open('temp.sqlite', 'wb') as f:
+                f.write(content)
+
+            db = SQLDatabase.from_uri("sqlite:///temp.sqlite")    
+            llm = OpenAI(temperature=0)
+            db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True, return_intermediate_steps=True)
+            result = db_chain(query)
+
+            st.write("Resposta:")
+            st.write(result['result'])
+            st.write("SQLQuery:")
+            st.write(result['intermediate_steps'][0])
+else:
+    #agent = create_csv_agent(OpenAI(temperature=0), 'titanic.csv', verbose=True)
